82dbaac55f49eff2a857e258d4f7fa3d3eb48195
raymonafa <raymonafa@gmail.com>
2021-06-26 14:52:05 +0700

    mona add konten-success

---
+++ resources/views/content/konten-success.blade.php
@@ -0,0 +1,32 @@
+@extends('index')
+
+@section('content')
+<div class="col-12 col-lg-4 col-md-4 offset-md-1 align-self-center center-bar text-center justify-content-center d-flex">
+    <div class="card">
+
+        <div class="card-body">
+
+            <div class="imgIbinstaforex">
+                <img src="assets/img/gambar1.png" alt="" style="width:100%; border-radius:8px;">
+            </div>
+
+            <p>I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.</p>
+
+
+
+            <div class="col-md-12 text-center">
+                <div class="row">
+                    <div class="col text-center">
+                        <a href="#" class="btn btn-primary btn-lg">Centered button</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+</div>
+
+
+@endsection
