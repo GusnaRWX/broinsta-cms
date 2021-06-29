c90a4dc51e340e6b61252bd1ff5c5ba4f6ed2e21
raymonafa <raymonafa@gmail.com>
2021-06-29 20:17:35 +0700

    mona add konten-withdraw,validasi,accountinformation

--- resources/views/content/konten-success.blade.php
+++ resources/views/content/konten-success.blade.php
@@ -10,17 +10,91 @@
                 <img src="assets/img/gambar1.png" alt="" style="width:100%; border-radius:8px;">
             </div>
 
-            <p>I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.</p>
+            <div class="card-form">
+                                    <h4 class="card-text">LengKapi Data dibawah untuk mevalidasi akun anda di BROINSTA. </h4>
+                                    <hr>
+                                    <form id="formDeposit" autocomplete="off" enctype="multipart/form-data" action="{{route('deposit.index')}}" method="POST">
+                                        @csrf @method('POST')
+                                        <div class="form-group">
+                                            <label for="inputAddress">Nomor akun Broninsta</label>
+                                            <input type="text" class="form-control" id="inputAddress" placeholder="Nomor akun instaforex" name="account_number">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputAddress">Password</label>
+                                            <input type="password" class="form-control" id="inputAddress" placeholder="Nomor akun instaforex" name="password">
+                                        </div>
+
+                                        <div class="form-row">
+                                            <div class="form-group col-md-12">
+                                                <label for="inputEmail4">Email</label>
+                                                <input type="email" class="form-control" id="inputEmail4" placeholder="Email" name="email">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-row">
+                                            <div class="form-group col-md-12">
+                                                <label for="inputEmail4">Nomor Telepon</label>
+                                                <input type="text" class="form-control" placeholder="Nomor telepon anda" name="phone">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-row">
+                                            <div class="form-group col-md-4">
+                                                <label for="inputState">WD Kirim Ke</label>
+                                                <select class="custom-select" name="withdraw">
+                                                    <option selected value="Bank Syariah"><span>Bank Syariah</span></option>
+                                                    <option selected value="Bank Mandiri"><span>Bank Mandiri</span></option>
+                                                    <option selected value="Bank BCA"><span>Bank BCA</span></option>
+                                                    <option selected value="Bank BNI"><span>Bank BNI</span></option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-8">
+                                                <label for="inputCity">Nomor Rekening</label>
+                                                <input type="text" class="form-control" id="inputCity" name="no_rek">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-row">
+                                            <div class="form-group col-md-12">
+                                                <label for="inputEmail4">Pemilik Nomor Rekening</label>
+                                                <input type="text" class="form-control" placeholder="Pemilik rekening" name="pemilik_rek">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-row">
+                                            <div class="form-group col-md-4">
+                                                <label for="inputState">Rebate Kirim Ke</label>
+                                                <select class="custom-select" name="withdraw">
+                                                    <option selected value="Bank Syariah"><span>Akun insta</span></option>
+                                                    <option selected value="Bank Mandiri"><span>Bank Mandiri</span></option>
+                                                    <option selected value="Bank BCA"><span>Bank BCA</span></option>
+                                                    
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-8">
+                                                <label for="inputCity">Nomor Rekening</label>
+                                                <input type="text" class="form-control" id="inputCity" name="no_rek">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-row">
+                                            <div class="form-group col-md-12">
+                                                <label for="inputEmail4">Pemilik Nomor Rekening Rebate</label>
+                                                <input type="text" class="form-control" placeholder="Pemilik rekening" name="pemilik_rek_rebate">
+                                            </div>
+                                        </div>
 
 
+                                        <div class="form-group">
+                                            <label for="inputAddress">KTP penerima Rebate</label>
+                                            <input type="text" class="form-control" id="inputAddress" placeholder="KTP Penerima" name="_ktp_penerima">
+                                        </div>
 
-            <div class="col-md-12 text-center">
-                <div class="row">
-                    <div class="col text-center">
-                        <a href="#" class="btn btn-primary btn-lg">Centered button</a>
-                    </div>
-                </div>
-            </div>
+                                        <br>
+                                        <button type="submit" class="btn btn-primary btn-lg">Validasi sekarang</button>
+                                        <!--handle menggunakan js-->
+                                    </form>
+                                </div>
 
 
         </div>
---
+++ resources/views/content/konten-withdraw.blade.php
@@ -0,0 +1,85 @@
+@extends('index')
+
+@section('content')
+ <!-- middle content -->
+ <div class="col-12 col-lg-4 col-md-4 offset-md-1 align-self-center center-bar  justify-content-center d-flex">
+    <div class="card">
+
+        <div class="card-body">
+
+            <div class="imgIbinstaforex text-center">
+                <img src="assets/img/gambar1.png" alt="" style="width:50%; border-radius:8px;">
+            </div>
+
+            <div class="card-form">
+                <h3 class="card-title mb-3">Withdraw</h3>
+                <h6 class="card-text">Lengkapi Data dibawah untuk penarikan uang akun anda di BROINSTA. </h6>
+                <hr>
+                <form id="formDeposit" autocomplete="off" enctype="multipart/form-data" action="{{route('deposit.index')}}" method="POST">
+                    @csrf @method('POST')
+                    <div class="form-group">
+                        <label for="inputAddress">Nomor akun Broninsta</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="Nomor akun instaforex" name="account_number">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputAddress">Password</label>
+                        <input type="password" class="form-control" id="inputAddress" placeholder="Nomor akun instaforex" name="password">
+                    </div>
+
+                    <div class="form-group ">
+                        <label for="inputEmail4">Jumlah Withdrawal (Min. USD1)</label>
+                        <input type="text" class="form-control" id="inputEmail4" placeholder="Email" name="withdraw">
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="inputEmail4">Email</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Email" name="email">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="inputEmail4">Nomor Telepon</label>
+                            <input type="text" class="form-control" placeholder="Nomor telepon anda" name="phone">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="inputState"> Kirim Ke</label>
+                            <select class="custom-select" name="withdraw">
+                                <option selected value="Bank Syariah"><span>Bank Syariah</span></option>
+                                <option selected value="Bank Mandiri"><span>Bank Mandiri</span></option>
+                                <option selected value="Bank BCA"><span>Bank BCA</span></option>
+                                <option selected value="Bank BNI"><span>Akun insta</span></option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-8">
+                            <label for="inputCity">Nomor Rekening</label>
+                            <input type="text" class="form-control" id="inputCity" name="no_rek">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="inputEmail4">Pemilik Nomor Rekening</label>
+                            <input type="text" class="form-control" placeholder="Pemilik rekening" name="pemilik_rek">
+                        </div>
+                    </div>
+
+                    <br>
+                    <button type="submit" class="btn btn-primary btn-lg">Withdraw sekarang</button>
+                    <!--handle menggunakan js-->
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+
+
+@endsection
